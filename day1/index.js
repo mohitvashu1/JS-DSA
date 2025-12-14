94dd8455467b83174f705d851b84b265bd8e09ec
mohitvashu1 <mohitchoubeybca@gmail.com>
2025-12-14 16:56:43 +0530

    DSA Challange 100 days

---
+++ day1/index.js
@@ -0,0 +1,16 @@
+let productPrice = 100;
+// let productPrice = "$100";
+// console.log(productPrice);
+
+let productDetails ={
+    name: "Laptop",
+    price: 100000,
+    discount:5000,
+    description: "A high-performance laptop",
+    inStock: true
+    
+
+}
+
+let names = productDetails.name
+console.log(names)
---
+++ day2/evenOdd.js
@@ -0,0 +1,10 @@
+ let a = 5 
+
+ function isEvenOrOdd (){
+    if (a%2==0){
+        return "even";
+    }else if(a%2==1){
+       return "odd"
+    }
+ }
+ console.log(isEvenOrOdd())
