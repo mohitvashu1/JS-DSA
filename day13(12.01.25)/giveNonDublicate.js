9592f3f6aeb0f8edd23b3a23d29a05a93e25335f
mohitvashu1 <mohitchoubeybca@gmail.com>
2026-01-13 22:54:34 +0530

    xor

--- day13(12.01.25)/giveNonDublicate.js
+++ day13(12.01.25)/giveNonDublicate.js
@@ -1,15 +1,26 @@
-let arr = [1,1,2,2,4,5,5,69,78,78]
+let arr = [1,1,2,2,4,5,5,4,11]
 
-function returnX(){
-    let x=0
-    for(let i=0;i<arr.length;i++){
-        if(arr[i]!==arr[x]){
-            arr[x] = arr[i]
+// function returnX(){
+//     let x=0
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]!==arr[x]){
+//             arr[x] = arr[i]
             
               
-        }
-    }
-    return arr
+//         }
+//     }
+//     return arr
+// }
+
+// console.log(returnX());
+
+function returnX(){
+
+let ans = 0;
+for(i=0;i<arr.length;i++){
+    ans ^=arr[i]
+}
+return ans
 }
 
 console.log(returnX());
